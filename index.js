d59043aa406de5504375a6e08e28b68c360fe8fa
shrutika ramadurai <shrutikaece.nitw@gmail.com>
2022-12-24 18:35:57 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+
+var randomNumber1=Math.floor((Math.random()*6)+1);
+
+var randomImageSource="images/dice"+randomNumber1+".png";
+document.querySelector("img.img1").setAttribute("src",randomImageSource);
+
+var randomNumber2=Math.floor((Math.random()*6)+1);
+var randomImageSource2="images/dice"+randomNumber2+".png";
+document.querySelector("img.img2").setAttribute("src",randomImageSource2);
+if (randomNumber1===randomNumber2)
+{
+  document.querySelector("h1").innerHTML="Draw!";
+}
+else if (randomNumber1>=randomNumber2)
+{
+  document.querySelector("h1").innerHTML="Player 1 wins";
+}
+else
+{
+  document.querySelector("h1").innerHTML="Player 2 wins";
+}
